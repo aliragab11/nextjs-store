b48b4e752459680f5e1616c4be87807f627f9495
aliragab11 <ali.ragab@fehris.io>
2025-01-14 01:57:34 +0200

    add pages

---
+++ app/favorites/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FavoritesPage() {
+  return (
+    <div>Favourites</div>
+  )
+}
+
+export default FavoritesPage
---
+++ app/reviews/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ReviewsPagw() {
+  return <div>ReviewsPagw</div>;
+}
+
+export default ReviewsPagw;
