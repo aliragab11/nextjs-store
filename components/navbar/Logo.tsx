f0f15cd3d3c4fbf0d5620414effd3b92815f17c0
aliragab11 <ali.ragab@fehris.io>
2025-01-14 03:12:34 +0200

    add logo component

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import { VscVscode } from 'react-icons/vsc';
+import Link from 'next/link';
+import { Button } from '../ui/button';
 
 function Logo() {
-  return <div>Logo</div>;
+  return (
+    <Button asChild size="icon">
+      <Link href="/">
+        <VscVscode className="h-6 w-6" />
+      </Link>
+    </Button>
+  );
 }
 
 export default Logo;
