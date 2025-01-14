1667dfc1e4dd11d258a936f24984da8456b69f3e
aliragab11 <ali.ragab@fehris.io>
2025-01-14 03:43:16 +0200

    shadecn change theme

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 function HomePage() {
   return (
     <div>
-      <h1 className="text-3xl">HomePage</h1>
-      <Button variant="default" size="lg" className="capitalize m-8">
+      <h1 className="text-3xl text-muted-foreground">HomePage</h1>
+      <Button variant="outline" size="lg" className="capitalize m-8">
         Click me
       </Button>
       <Button asChild>
