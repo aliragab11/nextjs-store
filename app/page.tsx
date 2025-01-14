a17013f30712b0228f4e1bc757c7566f458a037d
aliragab11 <ali.ragab@fehris.io>
2025-01-14 02:29:01 +0200

    add shadecn ui components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,17 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 function HomePage() {
-  return <h1 className="text-3xl">HomePage</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl">HomePage</h1>
+      <Button variant="default" size="lg" className="capitalize m-8">
+        Click me
+      </Button>
+      <Button asChild>
+        <Link href="/about">about</Link>
+      </Button>
+    </div>
+  );
 }
 export default HomePage;
