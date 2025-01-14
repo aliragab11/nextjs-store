21761f482c74239cc7e9f1edbfc05e22db4de4df
aliragab11 <ali.ragab@fehris.io>
2025-01-14 02:59:14 +0200

    add navbar component blueprint

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Navbar from '@/components/navbar/Navbar';
+import Container from '@/components/global/Container';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${inter.className} antialiased`}>
-      <body>{children}</body>
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>
+        <Navbar />
+        <Container className="py-20">{children}</Container>
+      </body>
     </html>
   );
 }
