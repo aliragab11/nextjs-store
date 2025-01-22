984fcf425e0f7076cdf6528ec3d09ad1e6815ac4
aliragab11 <ali.ragab@fehris.io>
2025-01-23 01:44:19 +0200

    add toast

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,10 @@
 'use client';
+import { Toaster } from '@/components/ui/toaster';
 import { ThemeProvider } from './theme-provider';
 function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <Toaster />
       <ThemeProvider
         attribute="class"
         defaultTheme="system"
