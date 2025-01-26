a780000573fba302174ae20df2d6634568b32fc1
aliragab11 <ali.ragab@fehris.io>
2025-01-26 03:36:50 +0200

    Rating and Comment Components

--- components/reviews/Comment.tsx
+++ components/reviews/Comment.tsx
@@ -1,5 +1,32 @@
-function Comment() {
-  return <div>Comment</div>;
+'use client';
+
+import { useState } from 'react';
+import { Button } from '../ui/button';
+
+function Comment({ comment }: { comment: string }) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const toggleExpanded = () => {
+    setIsExpanded((prev) => !prev);
+  };
+
+  const longComment = comment.length > 130;
+  const displayComment =
+    longComment && !isExpanded ? `${comment.slice(0, 130)}...` : comment;
+
+  return (
+    <div>
+      <p className="text-sm">{displayComment}</p>
+      {longComment && (
+        <Button
+          onClick={toggleExpanded}
+          className="capitalize pl-0 text-muted-foreground"
+          variant={'link'}
+        >
+          {isExpanded ? 'show less' : 'show more'}
+        </Button>
+      )}
+    </div>
+  );
 }
 
 export default Comment;
